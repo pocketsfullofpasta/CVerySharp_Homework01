860046abe7325ae3f686cc35c048c73c65080054
Ivo <ivoirlabs@gmail.com>
2020-11-27 17:57:23 +0200

    Calculator

---
+++ Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Homework01
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ievadi skaitli x: ");
+            string skaitlis1 = Console.ReadLine().Replace(",", ".");
+            double x = Convert.ToDouble(skaitlis1);
+
+            Console.Write("Ievadi skaitli y: ");
+            string skaitlis2 = Console.ReadLine().Replace(",", ".");
+            double y = Convert.ToDouble(skaitlis2);
+
+            Console.Write("Ievadi aritmētisko operāciju (+, -, / vai *): ");
+            string z = Console.ReadLine().Trim();
+
+            switch (z)
+            {
+                case "+":
+                    Console.WriteLine(x + " " + z + " " + y + " = " + Utils.Plus(x, y));
+                    break;
+
+                case "-":
+                    Console.WriteLine(x + " " + z + " " + y + " = " + Utils.Minus(x, y)); 
+                    break;
+
+                case "/":
+                    Console.WriteLine(x + " " + z + " " + y + " = " + Utils.Divide(x, y)); 
+                    break;
+
+                case "*":
+                    Console.WriteLine(x + " " + z + " " + y + " = " + Utils.Multiply(x, y)); 
+                    break;
+
+                default:
+                    Console.WriteLine("Kļūda aritmētiskās operācijas ievadē!"); 
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework01
+{
+    class Utils
+    {
+        public static double Plus(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Minus(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+    }
+}
